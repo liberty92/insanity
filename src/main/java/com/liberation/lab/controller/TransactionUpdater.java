64a48ca0d296d865e1d98a97ea0cb48e37ff8098
Liberty <liberty4692@gmail.com>
2016-05-25 10:22:41 +0700

    update 2505 - fix unicode error

--- src/main/java/com/liberation/lab/controller/TransactionUpdater.java
+++ src/main/java/com/liberation/lab/controller/TransactionUpdater.java
@@ -1,7 +1,23 @@
 package com.liberation.lab.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.liberation.lab.model.Orders;
+import com.liberation.lab.service.OrdersService;
+
 public class TransactionUpdater implements Runnable{
 
+	private OrdersService ordersService;
+
+	@Autowired(required = true)
+	@Qualifier(value = "ordersService")
+	public void setOrdersService(OrdersService us) {
+		this.ordersService = us;
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -14,6 +30,10 @@ public class TransactionUpdater implements Runnable{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}*/
+		
+		List<Orders> list = this.ordersService.listOrders();
+		
+		System.out.println(list.get(0).toString());
 		
 	}
 
